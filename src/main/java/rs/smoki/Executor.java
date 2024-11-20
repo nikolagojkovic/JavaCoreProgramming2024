b862d15e0fc11d9ee7060ad455d6081d45ae84f5
nikolagojkovic <nikolagojko55@gmail.com>
2024-11-20 20:22:54 +0100

    Inicijalni commit

---
+++ src/main/java/rs/smoki/Executor.java
@@ -0,0 +1,7 @@
+package rs.smoki;
+
+public class Executor {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/rs/smoki/one/Person.java
@@ -0,0 +1,5 @@
+package rs.smoki.one;
+
+public class Person {
+    }
+}
